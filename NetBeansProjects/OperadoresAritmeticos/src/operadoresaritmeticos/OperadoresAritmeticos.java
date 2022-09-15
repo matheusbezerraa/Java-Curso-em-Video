bf1168926213cfd6722d63576db066ab255bb96b
Matheus Freire Bezerra <matheus.freire.bezerra@gmail.com>
2022-09-15 15:18:52 -0300

    commit funçoes aritmetricas

---
+++ NetBeansProjects/OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,20 @@
+
+package operadoresaritmeticos;
+
+
+public class OperadoresAritmeticos {
+
+   
+    public static void main(String[] args) {
+       
+        float n1 = 3;
+        float n2 = 10;
+        float m = (n1 + n2)/2;
+        System.out.println(m);
+        
+        
+        
+        
+    }
+    
+}
