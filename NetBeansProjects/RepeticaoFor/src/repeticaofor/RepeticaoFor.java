dc5822b97c19280ba5b1fd856207c3501b90fe23
Matheus Freire Bezerra <matheus.freire.bezerra@gmail.com>
2022-09-22 15:06:15 -0300

    commit aula 13

---
+++ NetBeansProjects/RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repeticaofor;
+
+/**
+ *
+ * @author Matheus Bezerra
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int cc=0; cc<=4;cc++){
+            System.out.println("cambalhota "+ cc);
+        }
+        int cc=0;
+        while(cc<4){
+            System.out.println("cambalhota "+ cc);
+            cc++;
+        }
+    }
+    
+}
