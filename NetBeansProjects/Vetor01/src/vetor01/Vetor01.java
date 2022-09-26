8dd3fd07b166db394696b3903dc97832465971d3
Matheus Freire Bezerra <matheus.freire.bezerra@gmail.com>
2022-09-26 17:48:53 -0300

    commit aula 14

---
+++ NetBeansProjects/Vetor01/src/vetor01/Vetor01.java
@@ -0,0 +1,21 @@
+
+package vetor01;
+
+/**
+ *
+ * @author Matheus Bezerra
+ */
+public class Vetor01 {
+
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n[] = {3,2,8,7,5,4};
+        System.out.println("total de casas de N é "+ n.length);
+        for(int c=0; c<=n.length -1; c++){
+            System.out.println("na posição "+ c +" temos o valor "+ n[c]);
+       }
+        
+    }
+    
+}
